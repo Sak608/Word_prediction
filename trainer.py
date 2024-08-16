a434b254e7da17feeaa230fa25fbdb4d987a36e6
SAKSHI RAJ <115283283+Sak608@users.noreply.github.com>
2024-08-16 15:11:15 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,49 +1,97 @@
-# Importing the Libraries
-from tensorflow.keras.models import load_model
-import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam
+from tensorflow import keras
+from keras.utils.vis_utils import plot_model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.callbacks import TensorBoard
+from IPython.display import Image 
 import pickle
+import numpy as np
+import os, string
 
-# Load the model and tokenizer
-model = load_model('nextword1.h5')
-tokenizer = pickle.load(open('tokenizer1.pkl', 'rb'))
+file = open("metamorphosis_clean.txt", "r", encoding = "utf8")
+lines = []
 
-def Predict_Next_Words(model, tokenizer, text):
-    for i in range(3):
-        sequence = tokenizer.texts_to_sequences([text])[0]
-        sequence = np.array(sequence)
-        
-        preds = model.predict_classes(sequence)
-#         print(preds)
-        predicted_word = ""
-        
-        for key, value in tokenizer.word_index.items():
-            if value == preds:
-                predicted_word = key
-                break
-        
-        print(predicted_word)
-        return predicted_word
+for i in file:
+    lines.append(i)
     
-# text1 = "at the dull"
-# text2 = "collection of textile"
-# text3 = "what a strenuous"
-# text4 = "stop the script"
+print("The First Line: ", lines[0])
+print("The Last Line: ", lines[-1])
 
-while(True):
-
-    text = input("Enter your line: ")
+# cleaning the data
+data = ""
+for i in lines:
+    data = ' '. join(lines)
     
-    if text == "stop the script":
-        print("Ending The Program.....")
-        break
-    
-    else:
-        try:
-            text = text.split(" ")
-            text = text[-1]
+data = data.replace('\n', '').replace('\r', '').replace('\ufeff', '')
 
-            text = ''.join(text)
-            Predict_Next_Words(model, tokenizer, text)
-            
-        except:
-            continue
+translator = str.maketrans(string.punctuation, ' '*len(string.punctuation)) #map punctuation to space
+new_data = data.translate(translator)
+
+z = []
+for i in data.split():
+    if i not in z:
+        z.append(i)  
+data = ' '.join(z)
+
+# tokenization
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([data])
+
+# saving the tokenizer for predict function.
+pickle.dump(tokenizer, open('tokenizer1.pkl', 'wb'))
+sequence_data = tokenizer.texts_to_sequences([data])[0]
+vocab_size = len(tokenizer.word_index) + 1
+
+sequences = []
+
+for i in range(1, len(sequence_data)):
+    words = sequence_data[i-1:i+1]
+    sequences.append(words)
+    
+print("The Length of sequences are: ", len(sequences))
+sequences = np.array(sequences)
+
+X = []
+y = []
+
+for i in sequences:
+    X.append(i[0])
+    y.append(i[1])
+    
+X = np.array(X)
+y = np.array(y)
+print("The Data is: ", X[:5])
+print("The responses are: ", y[:5])
+
+# creating the model
+model = Sequential()
+model.add(Embedding(vocab_size, 10, input_length=1))
+model.add(LSTM(1000, return_sequences=True))
+model.add(LSTM(1000))
+model.add(Dense(1000, activation="relu"))
+model.add(Dense(vocab_size, activation="softmax"))
+# model.summary()
+
+#plotting the model
+keras.utils.plot_model(model, to_file='model.png', show_layer_names=True)
+
+checkpoint = ModelCheckpoint("nextword1.h5", monitor='loss', verbose=1,
+    save_best_only=True, mode='auto')
+
+reduce = ReduceLROnPlateau(monitor='loss', factor=0.2, patience=3, min_lr=0.0001, verbose = 1)
+
+logdir='logsnextword1'
+tensorboard_Visualization = TensorBoard(log_dir=logdir)
+
+# compile and fit the model
+model.compile(loss="categorical_crossentropy", optimizer=Adam(lr=0.001))
+model.fit(X, y, epochs=150, batch_size=64, callbacks=[checkpoint, reduce, tensorboard_Visualization])
+
+pil_img = Image(filename='graph1.png')
+display(pil_img)
